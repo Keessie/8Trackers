d9f98a84b76aaf2b2ac367409f008abe28f398bc
whroman <me@walterroman.com>
2016-09-22 11:25:17 +0200

    added trackers script to index.html

--- js/globals.js
+++ js/globals.js
@@ -7,14 +7,3 @@ var MAP_CENTER = {
     lat: 51.998276,
     lng: 4.353702
 };
-
-var TRACKERS = [
-    new Tracker(0, 13, '8E67FD'),
-    new Tracker(1, 14, 'FD7567'),
-    new Tracker(2, 15, '6991FD'),
-    new Tracker(3, 16, '00E64D'),
-    new Tracker(4, 17, '65DBDB'),
-    new Tracker(5, 18, 'FF9900'),
-    new Tracker(6, 19, 'E661AC'),
-    new Tracker(7, 20, 'FDF569')
-];
