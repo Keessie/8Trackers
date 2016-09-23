b2a5e56735f9c5ab532e68de848bbb42ab15da65
whroman <me@walterroman.com>
2016-09-23 17:09:55 +0200

    removing "center" button in map view

--- js/ui/mapControls.js
+++ js/ui/mapControls.js
@@ -16,42 +16,14 @@ function renderMap() {
 }
 
 function renderMapControls(map) {
-    var centerControlDiv = renderMapControlCenter(map);
     var clearPathsControlDiv = renderMapControlClearPaths();
     var listViewControlDiv = renderMapControlListView();
-
-    centerControlDiv.index = 1;
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 
     clearPathsControlDiv.index = 2;
     map.controls[google.maps.ControlPosition.TOP_RIGHT].push(clearPathsControlDiv);
 
     clearPathsControlDiv.index = 3;
-    map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(listViewControlDiv);
-}
-
-function renderMapControlCenter(map) {
-    var controlDiv = document.createElement('div');
-
-    // Set CSS for the control border.
-    var controlUI = document.createElement('div');
-    controlUI.className = 'map-control-button';
-    controlUI.title = 'recenter the map';
-    controlDiv.appendChild(controlUI);
-
-    // Set CSS for the control interior.
-    var controlText = document.createElement('div');
-    controlText.className = 'map-control-button-text';
-    controlText.innerHTML = 'Center T14';
-    controlUI.appendChild(controlText);
-
-    // Setup the click event listeners: simply set the map to Chicago.
-    controlUI.addEventListener('click', function() {
-        var centeringCoords = TRACKERS.getByMapIndex(18).googleLatLng || DEFAULT_CENTER_COORDS;
-        map.setCenter(centeringCoords);
-    });
-
-    return controlDiv;
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(listViewControlDiv);
 }
 
 function renderMapControlClearPaths() {
