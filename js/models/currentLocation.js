c67f9023f8b119a7999cac5293b61991e181ea55
whroman <me@walterroman.com>
2016-09-27 17:47:57 +0200

    fixing bug in currentLocation model

--- js/models/currentLocation.js
+++ js/models/currentLocation.js
@@ -1,17 +1,18 @@
 /* global google */
 /* exported currentLocation */
 
-var markerIcon = new google.maps.MarkerImage(
-    '//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
-    new google.maps.Size(22, 22),
-    new google.maps.Point(0, 18),
-    new google.maps.Point(11, 11)
-);
-
 var currentLocation = {
     marker: null,
     googleLatLng: null,
     setMarker: function(lat, lng, map) {
+        // `markerIcon` must be declared here because `google` isn't available in the global scope on-app-load
+        var markerIcon = new google.maps.MarkerImage(
+            '//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+            new google.maps.Size(22, 22),
+            new google.maps.Point(0, 18),
+            new google.maps.Point(11, 11)
+        );
+
         if (this.marker) this.marker.setMap(null); // delete old marker
 
         this.googleLatLng = new google.maps.LatLng(lat, lng);
